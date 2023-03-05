68537062b3e09e777ec93df3e1b09c2bb2e0bc8c
Coding Chimp <iferca@nuvadi.com>
2023-03-05 17:22:34 +0100

    Database drafted

--- 4.WebDevelopment/mysite/mysite/settings.py
+++ 4.WebDevelopment/mysite/mysite/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'greeter.apps.GreeterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
