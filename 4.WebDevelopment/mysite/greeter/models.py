7d7badb96ca1e98df63a597bd60622280b516e26
Coding Chimp <iferca@nuvadi.com>
2023-03-06 22:09:04 +0100

    Django sample add completed

--- 4.WebDevelopment/mysite/greeter/models.py
+++ 4.WebDevelopment/mysite/greeter/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, default='Mx')
+    
+    def __str__(self):
+        return ' '.join((self.title, self.name))
+    
+    def is_male(self):
+        return self.title.lower() in ['mr', 'master']
+    
+    def is_female(self):
+        return self.title.lower() not in ['mx'] and not self.is_male()
--- 4.WebDevelopment/mysite/greeter/urls.py
+++ 4.WebDevelopment/mysite/greeter/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('greet/<int:user_id>', views.greet, name='greet')
 ]
