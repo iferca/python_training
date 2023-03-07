5e6265b19e2ecafc36c4c244319bd3fc6cec36b8
eriklueh <github.e16bv@simplelogin.co>
2023-03-07 06:41:58 -0300

    5.Pygame updated with new documentation and character can shoot

--- 5.Pygame/gamespy/Game/platform_game.py
+++ 5.Pygame/gamespy/Game/platform_game.py
@@ -5,72 +5,142 @@ pg.init()
 pg.display.set_caption("First Game")
 
 
-class Config:
-    screen_height = 500
-    screen_width = 500
-    width_of_rec = 64
-    height_of_rec = 64
-    position_on_x = 50
-    position_on_y = 50
-    speed = 5
-    black = (0, 0, 0)
-    red = (255, 0, 0)
-    jump_count = 10
-    is_jump = True
-    walkRight = [pg.image.load(path.join("Right", f'R{n}.png')) for n in range(1, 10)]
-    walkLeft = [pg.image.load(path.join("Left", f'L{n}.png')) for n in range(1, 10)]
-    char = pg.image.load('standing.png')
-    bg = pg.image.load('bg.jpg')
+class Player:
+    position_on_x = 300
+    position_on_y = 410
+    width = 64
+    height = 64
     right = False
     left = False
     walk_count = 0
+    jump_count = 10
+    is_jump = True
+    speed = 5
+    walk_right = [pg.image.load(path.join("Right", f'R{n}.png')) for n in range(1, 10)]
+    walk_left = [pg.image.load(path.join("Left", f'L{n}.png')) for n in range(1, 10)]
+    char = pg.image.load(path.join('standing.png'))
+    standing = False
 
 
-def draw_rectangle(screen):
-    pg.draw.rect(screen, Config.red,
-                 (Config.position_on_x, Config.position_on_y, Config.width_of_rec, Config.height_of_rec))
+class Config:
+    screen_height = 500
+    screen_width = 500
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    clock = pg.time.Clock()
+    bg = pg.image.load(path.join("bg.jpg"))
+
+
+class Projectile(object):
+    def __init__(self, position_on_x, position_on_y, radius, color, facing):
+        self.position_on_x = position_on_x
+        self.position_on_y = position_on_y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.speed = 8 * facing
+
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, (self.position_on_x, self.position_on_y), self.radius)
+
+
+def draw_player(screen):
+    if Player.walk_count + 1 >= 27:
+        Player.walk_count = 0
+    if Player.left:
+        screen.blit(Player.walk_left[Player.walk_count // 3], (Player.position_on_x, Player.position_on_y))
+        Player.walk_count += 1
+    elif Player.right:
+        screen.blit(Player.walk_right[Player.walk_count // 3], (Player.position_on_x, Player.position_on_y))
+        Player.walk_count += 1
+    else:
+        if Player.right:
+            screen.blit(Player.walk_right[0], (Player.position_on_x, Player.position_on_y))
+
+        else:
+            screen.blit(Player.walk_left[0], (Player.position_on_x, Player.position_on_y))
+
+
+def draw_game(screen, bullets):
+    screen.blit(Config.bg, (0, 0))
+    for bullet in bullets:
+        bullet.draw(screen)
+    draw_player(screen)
     pg.display.update()
 
 
-def check_lateral_move(keys):
-    if keys[pg.K_LEFT] and Config.position_on_x > Config.speed:
-        Config.position_on_x -= Config.speed
+def check_move(keys):
+    if keys[pg.K_LEFT] and Player.position_on_x > Player.speed:
+        Player.position_on_x -= Player.speed
+        Player.left = True
+        Player.right = False
+        Player.standing = False
 
-    if keys[pg.K_RIGHT] and Config.position_on_x < Config.screen_width - Config.width_of_rec:
-        Config.position_on_x += Config.speed
+    elif keys[pg.K_RIGHT] and Player.position_on_x < Config.screen_width - Player.width:
+        Player.position_on_x += Player.speed
+        Player.right = True
+        Player.left = False
+        Player.standing = False
+
+    else:
+        Player.standing = False
+        Player.walk_count = 0
 
 
 def jump(keys):
-    if Config.is_jump:
+    if Player.is_jump:
 
-        if keys[pg.K_SPACE]:
-            Config.is_jump = False
+        if keys[pg.K_UP]:
+            Player.is_jump = False
+            Player.right = False
+            Player.left = False
+            Player.walk_count = 0
+
     else:
-        if Config.jump_count >= -10:
+        if Player.jump_count >= -10:
             neg = 1
-            if Config.jump_count < 0:
+            if Player.jump_count < 0:
                 neg = -1
-            Config.position_on_y -= (Config.jump_count ** 2) / 2 * neg
-            Config.jump_count -= 1
+            Player.position_on_y -= (Player.jump_count ** 2) / 2 * neg
+            Player.jump_count -= 1
         else:
-            Config.is_jump = True
-            Config.jump_count = 10
+
+            Player.is_jump = True
+            Player.jump_count = 10
 
 
 def run_game():
+    bullets = []
     run = True
     screen = pg.display.set_mode((Config.screen_width, Config.screen_height))
     while run:
-        pg.time.delay(100)
+
+        Config.clock.tick(27)
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
 
+        for bullet in bullets:
+            if 500 > bullet.position_on_x > 0:
+                bullet.position_on_x += bullet.speed
+            else:
+                bullets.pop(bullets.index(bullet))
         keys = pg.key.get_pressed()
+        if keys[pg.K_SPACE]:
+            if Player.left:
+                facing = -1
+            else:
+                facing = 1
+
+            if len(bullets) < 3:
+                bullets.append(
+                    Projectile(round(Player.position_on_x + Player.width // 2),
+                               round(Player.position_on_y + Player.height // 2), 6, Config.black,
+                               facing))
         jump(keys)
-        check_lateral_move(keys)
-        draw_rectangle(screen)
-        screen.fill(Config.black)
+        check_move(keys)
+        draw_game(screen, bullets)
     pg.quit()
 
 
