bf304a7a0790fc8745836ee9f9100ea8f57b4d30
Coding Chimp <iferca@nuvadi.com>
2023-03-09 09:56:00 +0100

    Game fixed

--- 5.Pygame/gamespy/Game/platform_game.py
+++ 5.Pygame/gamespy/Game/platform_game.py
@@ -2,6 +2,7 @@ import pygame as pg
 from os import path
 
 pg.init()
+pg.font.init()
 pg.display.set_caption("First Game")
 
 
@@ -31,21 +32,30 @@ class Enemy(object):
         self.position_on_y = position_on_y
         self.width = width
         self.height = height
-        self.path = [position_on_x, end]  # This will define where our enemy starts and finishes their path.
+        self.path = [position_on_x, end]
         self.walk_count = 0
         self.speed = 3
+        self.hitbox = (self.position_on_x + 17, self.position_on_y + 2, 31, 57)  # NEW
+        self.health = 10  # NEW
+        self.visible = True  # NEW
 
     def draw(self, screen):
         self.move()
-        if self.walk_count + 1 >= 33:
-            self.walk_count = 0
+        if self.visible:  # NEW
+            if self.walk_count + 1 >= 33:
+                self.walk_count = 0
 
-        if self.speed > 0:
-            screen.blit(self.walk_right[self.walk_count // 3], (self.position_on_x, self.position_on_y))
-            self.walk_count += 1
-        else:
-            screen.blit(self.walk_left[self.walk_count // 3], (self.position_on_x, self.position_on_y))
-            self.walk_count += 1
+            if self.speed > 0:
+                screen.blit(self.walk_right[self.walk_count // 3], (self.position_on_x, self.position_on_y))
+                self.walk_count += 1
+            else:
+                screen.blit(self.walk_left[self.walk_count // 3], (self.position_on_x, self.position_on_y))
+                self.walk_count += 1
+            pg.draw.rect(screen, (255, 0, 0), (self.hitbox[0], self.hitbox[1] - 20, 50, 10))  # NEW
+            pg.draw.rect(screen, (0, 128, 0),
+                         (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (10 - self.health)), 10))  # NEW
+            self.hitbox = (self.position_on_x + 17, self.position_on_y + 2, 31, 57)
+            # pygame.draw.rect(win, (255,0,0), self.hitbox,2)
 
     def move(self):
         if self.speed > 0:
@@ -63,6 +73,13 @@ class Enemy(object):
                 self.position_on_x += self.speed
                 self.walk_count = 0
 
+    def hit(self):  # ALL NEW
+        if self.health > 0:
+            self.health -= 1
+        else:
+            self.visible = False
+        print('hit')
+
 
 class Config:
     screen_height = 500
@@ -71,6 +88,7 @@ class Config:
     red = (255, 0, 0)
     clock = pg.time.Clock()
     bg = pg.image.load(path.join("bg.jpg"))
+    score = 0
 
 
 class Projectile(object):
@@ -103,11 +121,19 @@ def draw_player(screen):
             screen.blit(Player.walk_left[0], (Player.position_on_x, Player.position_on_y))
 
 
+def print_font(screen):
+    comicsans_font = pg.font.SysFont("comicsans", 30, True)
+    text = comicsans_font.render("Score: " + str(Config.score), 1,
+                                 Config.black)  # Arguments are: text, anti-aliasing, color
+    screen.blit(text, (390, 10))
+
+
 def draw_game(screen, bullets, goblin):
     screen.blit(Config.bg, (0, 0))
     goblin.draw(screen)
     for bullet in bullets:
         bullet.draw(screen)
+    print_font(screen)
     draw_player(screen)
     pg.display.update()
 
@@ -153,6 +179,7 @@ def jump(keys):
 
 
 def run_game():
+    shoot_loop = 0
     goblin = Enemy(100, 410, 64, 64, 300)
     bullets = []
     run = True
@@ -160,18 +187,31 @@ def run_game():
     while run:
 
         Config.clock.tick(27)
-
+        if shoot_loop > 0:
+            shoot_loop += 1
+        if shoot_loop > 3:
+            shoot_loop = 0
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
 
         for bullet in bullets:
+            if bullet.position_on_y - bullet.radius < goblin.hitbox[1] + goblin.hitbox[3] \
+                    and bullet.position_on_y + bullet.radius > goblin.hitbox[1]:
+
+                if bullet.position_on_x + bullet.radius > goblin.hitbox[0] and bullet.position_on_x - bullet.radius < \
+                        goblin.hitbox[0] + goblin.hitbox[2]:
+                    goblin.hit()
+                    Config.score += 1  # NEW
+                    bullets.pop(bullets.index(bullet))
+
             if 500 > bullet.position_on_x > 0:
                 bullet.position_on_x += bullet.speed
             else:
                 bullets.pop(bullets.index(bullet))
+
         keys = pg.key.get_pressed()
-        if keys[pg.K_SPACE]:
+        if keys[pg.K_SPACE] and shoot_loop == 0:
             if Player.left:
                 facing = -1
             else:
@@ -182,6 +222,8 @@ def run_game():
                     Projectile(round(Player.position_on_x + Player.width // 2),
                                round(Player.position_on_y + Player.height // 2), 6, Config.black,
                                facing))
+
+            shoot_loop = 1
         jump(keys)
         check_move(keys)
         draw_game(screen, bullets, goblin)
